c9475df80c840a4070e1f8fa786216165dcfbec5
渡邉雅紀 <masa101224@outlook.jp>
2026-05-12 22:47:52 +0900

    シーン追加　プレイヤーの移動の仕方変更

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -28,7 +29,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _moveInput = Input.GetAxisRaw("Horizontal");
+        _moveInput = 0;
+
+        if (Keyboard.current.aKey.isPressed)
+        {
+            _moveInput = -1;
+        }
+
+        if (Keyboard.current.dKey.isPressed)
+        {
+            _moveInput = 1;
+        }
         CheckGround();
         Jump();
         Attack();
