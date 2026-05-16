1f5f8a892088053ca844858db130c28f4e802d06
渡邉雅紀 <masa101224@outlook.jp>
2026-05-16 23:33:42 +0900

    ガチャシステム変更

--- Assets/Script/GachaManager.cs
+++ Assets/Script/GachaManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class GachaManager : MonoBehaviour
 {
@@ -6,28 +7,68 @@ public class GachaManager : MonoBehaviour
     public class GachaEntry
     {
         public MagicDataSO data;
-        public int rate;
     }
 
-    [SerializeField] GachaEntry[] table;
-    
+    [SerializeField] GachaEntry[] _table;
+    [SerializeField] int _SSR = 5;
+    [SerializeField] int _SR = 10;
+    [SerializeField] int _R = 50;
+
     public MagicDataSO GetRangdomMagic()
     {
-        int total = 0;
-        foreach(var t in table) 
-            total += t.rate;
-
-        int rand = Random.Range(0, total);
-        int current = 0;
-        foreach (var t in table)
+        int rand = Random.Range(0,100);
+        Rarity targetRarity;
+        //レア度の抽選
+        if(rand < _SSR)
         {
-            current += t.rate;
-            if(rand <= current)
+            targetRarity = Rarity.SSR;
+        }
+        else if(rand < _SR)
+        {
+            targetRarity = Rarity.SR;
+        }
+        else if(rand < _R)
+        {
+            targetRarity = Rarity.R;
+        }
+        else
+        {
+            targetRarity = Rarity.N;
+        }
+
+        //該当レア度の魔法を数える
+        int count = 0;
+        foreach (var t in _table)
+        {
+            if (t.data.rarity == targetRarity)
             {
-                return t.data;
+                count++;
             }
         }
-        return table[0].data;
+        //該当レア度が存在しないときの保険
+        if(count == 0)
+        {
+            return _table[0].data;
+        }
+
+        // レア度内ランダム
+        int pick = Random.Range(0, count);
+        int current = 0;
+
+        foreach (var t in _table)
+        {
+            if (t.data.rarity == targetRarity)
+            {
+                if (current == pick)
+                {
+                    return t.data;
+                }
+
+                current++;
+            }
+        }
+
+        return _table[0].data;
     }
 }
 
